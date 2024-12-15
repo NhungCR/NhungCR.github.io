b5ae418cec166dc007ff46cfc50ae0b8cc22d464
NhungCR <hongnhungmadridista@gmail.com>
2024-12-15 13:30:56 +0700

    dynamic name

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // MorphSVGPlugin.convertToPath("polygon");
 
-const volume = document.querySelector("#volume");
-const icon = document.querySelector("#volume > i");
-const audio = document.querySelector("audio");
-
-volume.addEventListener("click", () => {
-	if (audio.paused) {
-		audio.play();
-		icon.classList.remove('fa-volume-mute');
-		icon.classList.add('fa-volume-up');
-
-	} else {
-		audio.pause();
-		icon.classList.remove('fa-volume-up');
-		icon.classList.add('fa-volume-mute');
-	}
-});
-
-
 var xmlns = "http://www.w3.org/2000/svg",
 	xlinkns = "http://www.w3.org/1999/xlink",
 	select = function (s) {
